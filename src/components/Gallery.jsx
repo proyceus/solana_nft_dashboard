b3b769170f9db72eadd027d02226cd6ceff1770b
proyceus <proyceus@gmail.com>
2022-09-08 12:28:46 -0400

    added gallery component to view NFTs

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Gallery = () => {
+  return (
+    <section class="overflow-hidden text-gray-700 ">
+      <div class="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
+        <div class="flex flex-wrap -m-1 md:-m-2"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
