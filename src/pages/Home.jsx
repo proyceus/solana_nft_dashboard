3a9fd1eb81acec155127aed986c22d8d3ddcf1c3
proyceus <proyceus@gmail.com>
2022-09-08 11:42:14 -0400

    added search box component to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Search } from "../components";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="mt-12">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <Search />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
