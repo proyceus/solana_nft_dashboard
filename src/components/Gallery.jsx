fe7094f3672786ab1ed42bbf1e357a8375312908
proyceus <proyceus@gmail.com>
2022-09-08 13:13:22 -0400

    finished styling gallery and added NFT card view component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -9,13 +9,13 @@ const Gallery = () => {
       {walletTokens && (
         <section className="overflow-hidden text-gray-700 ">
           <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
-            <div className="flex flex-wrap -m-1 md:-m-2">
+            <div className=" flex flex-wrap -m-1 md:-m-2">
               {walletTokens.map((item, index) => (
                 <div className="flex flex-wrap w-1/3" key={index}>
                   <div className="w-full p-1 md:p-2">
                     <img
                       alt={item.name}
-                      className="block object-cover object-center w-full h-full rounded-lg"
+                      className=" object-cover w-full h-full rounded-lg"
                       src={item.image}
                     />
                   </div>
