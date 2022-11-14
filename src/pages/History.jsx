2947618c26132521a1393a1775db431f5052b04a
proyceus <proyceus@gmail.com>
2022-11-14 17:16:54 -0500

    adding imports

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { getTransactions } from "../helpers/helpers";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const History = () => {
   return <div>History</div>;
