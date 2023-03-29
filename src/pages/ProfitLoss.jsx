dfc6bed509c232ec49a2e96b2c82b1746efb3b46
proyceus <proyceus@gmail.com>
2023-03-29 12:26:20 -0400

    removed old code that is not needed anymore

--- src/pages/ProfitLoss.jsx
+++ src/pages/ProfitLoss.jsx
@@ -143,7 +143,6 @@ const ProfitLoss = () => {
   }
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
-      {/* <button onClick={loadData}>Load Data</button> */}
       <Header category="Page" title="ProfitLoss" />
       <GridComponent
         id="gridcomp"
